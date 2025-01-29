c3b4a2d65d73a4230621f3740f030980b2fa0823
Tatoline <taylan@yield.app>
2025-01-29 16:38:23 +0100

    User Controller Service Modal Route

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    country: { type: String, required: true },
+    gender: { type: String, required: true },
+    birthday: { type: Date, required: true }
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema)
