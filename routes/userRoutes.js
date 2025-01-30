092282c6c1f5e806a9cd1873ad0461c40ac5fd94
Tatoline <taylan@yield.app>
2025-01-30 17:37:18 +0100

    createUser func Added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const userController = require('../controllers/userController')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
 router.post('/isEmailExist', userController.checkEmailExistence)
+
+router.post('/createUser', 
+    [
+        body('name').notEmpty().withMessage('Name is required.'),
+        body('surname').notEmpty().withMessage('Surname is required.'),
+        body('email').isEmail().withMessage('Valid email is required.'),
+        body('country').notEmpty().withMessage('Country is required.'),
+        body('gender').notEmpty().withMessage('Gender is required.'),
+        body('birthday').notEmpty().withMessage('Birthday is required.'),
+        body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters.')
+    ],
+    userController.createUser
+)
 
 module.exports = router
